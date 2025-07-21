4b2f72641e2e14cd7fc64d0562ec534ab4dee254
aangelinaa <aangelinamariaa@gmail.com>
2025-07-21 15:34:59 -0300

    feat: conclusão do desafio sistema bancario

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -3,55 +3,79 @@ menu = """
 [d] Depositar
 [s] Sacar
 [e] Extrato
-[q] Sair
+[x] Sair
 
 => """
 
 saldo = 0
 limite = 500
 extrato = ""
-numero_saques = 0
+qtd_saques = 0
 LIMITE_SAQUES = 3
 
+
+print("Bem vindo ao AMS Sistema Bancário! O que vamos fazer hoje?")
 while True:
 
-    opcao = input(menu)
+    opcao = input(menu).lower()
 
     if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
 
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
+        valor = input("Informe o valor do depósito sem utilizar separadores decimais: ")
 
+        if not valor.isdigit():
+            print("Entrada inválida. Digite apenas números.")
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            if len(valor) == 1:
+                valor = float('0.0' + valor)
+            else:
+                valor = float(valor[:-2] + '.' + valor[-2:])
+
+            if valor > 0:
+                saldo += valor
+                extrato += f"Depósito: R$ {valor:.2f}\n"
+                print("Depósito realizado com sucesso!")
+            else:
+                print("Operação falhou! O valor do depósito deve ser maior que zero.")
 
     elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
-
-        excedeu_saldo = valor > saldo
-
-        excedeu_limite = valor > limite
-
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-        if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
-
-        elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
-
-        elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
-
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
-
+        valor = input("Informe o valor do saque sem utilizar separadores decimais: ")
+        
+        if not valor.isdigit() and valor != "":
+            print("Entrada inválida. Digite apenas números.")
         else:
-            print("Operação falhou! O valor informado é inválido.")
+
+            if len(valor) == 1:
+                valor = float('0.0' + valor)
+            else:
+                valor = float(valor[:-2] + '.' + valor[-2:])
+        
+            excedeu_saldo = valor > saldo
+            excedeu_limite = valor > limite
+            excedeu_saques = qtd_saques >= LIMITE_SAQUES
+
+            if excedeu_saldo:
+                print("Operação falhou! Você não tem saldo suficiente.")
+                print(f"\nSaldo disponível: R$ {saldo:.2f}")
+
+            elif excedeu_limite:
+                print("Operação falhou! O valor do saque excede o limite.")
+                print(f"\nLimite máximo por saque: R$ {limite:.2f}")
+
+            elif excedeu_saques:
+                print("Operação falhou! Número máximo de saques excedido.")
+                print(f"\nLimite de saques diários: {LIMITE_SAQUES}")
+
+
+            elif valor > 0:
+                saldo -= valor
+                extrato += f"Saque: R$ {valor:.2f}\n"
+                qtd_saques += 1
+                print("Saque realizado com sucesso!")
+
+            else:
+                print("Operação falhou! O valor do saque deve ser maior que zero.")
+
 
     elif opcao == "e":
         print("\n================ EXTRATO ================")
@@ -59,7 +83,8 @@ while True:
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
 
-    elif opcao == "q":
+    elif opcao == "x":
+        print("Obrigado por utilizar o sistema AMS.")
         break
 
     else:
